eb65e81d16dd7f92925714978e975141aed28058
Mukul-Shrisha051020 <73692643+Mukul-Shrisha051020@users.noreply.github.com>
2021-01-27 08:12:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+prediction = "";
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 15000
+
+});
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + ' "> ';
+    });
+}
+
+console.log("ml5 version", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/cgvabd56Q/model.json", model_loaded);
